ff513ed2bef365b000393a674f736978295b3753
larikich <laryssakich@gmail.com>
2024-09-09 10:42:58 -0300

    Adicionada função para troca dos banners.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Função para adicionar a classe 'on-scroll' ao header quando o scroll da página for maior que 50px
 window.addEventListener('scroll', function() {
     var header = document.getElementById('header');
     if (window.scrollY > 50) {
@@ -6,3 +7,16 @@ window.addEventListener('scroll', function() {
         header.classList.remove('on-scroll');
     }
 });
+
+// Função para alterar o banner da tela de produtos em um intervalo de tempo
+const banners = ["../images/banner-produtos.svg", "../images/banner-produtos2.png", "../images/banner-produtos3.png"];
+
+let bannerIndex = 0;
+
+function changeBanner() {
+    bannerIndex = (bannerIndex + 1) % banners.length;
+    document.querySelector('.products-banner-img').src = banners[bannerIndex];
+}
+
+setInterval(changeBanner, 3000);
+
